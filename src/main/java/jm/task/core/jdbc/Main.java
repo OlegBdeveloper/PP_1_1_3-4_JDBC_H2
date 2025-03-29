fc6daeb294ed524989a6b2948dc2b4e17fc6759a
OlegB <oleg89115498683@yandex.ru>
2025-03-30 00:06:26 +0300

    Решение

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,27 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserService userService = new UserServiceImpl();
+
+        userService.createUsersTable();
+
+        userService.saveUser("John", "Doe", (byte) 25);
+        userService.saveUser("Jane", "Smith", (byte) 30);
+        userService.saveUser("Alice", "Johnson", (byte) 28);
+        userService.saveUser("Bob", "Brown", (byte) 35);
+
+        userService.getAllUsers().forEach(System.out::println);
+
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
+
+
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,27 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mysql://localhost:3306/mydbtest";
+    private static final String USER = "root";
+    private static final String PASSWORD = "root";
+
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver"); // Загружаем драйвер
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Не найден", e);
+        }
+    }
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException("Ошибка", e);
+        }
+    }
 }
